3939165a8007fa2dc12c4b4eda0dc499a803e353
okradzemirian <okradzemirian@gmail.com>
2019-08-31 14:23:49 +0400

    Added PublicRoute

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Switch } from 'react-router-dom'
 import Firebase from '../components/Firebase/Firebase'
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 import Home from '../components/Home/Home'
 import LoginForm from '../components/LoginForm/LoginForm'
 import SignupForm from '../components/SignupForm/SignupForm'
@@ -11,8 +12,8 @@ const Router = () => (
         <Firebase>
             <Switch>
                 <PrivateRoute exact path="/" component={Home} />
-                <Route path="/login" component={LoginForm} />
-                <Route path="/signup" component={SignupForm} />
+                <PublicRoute exact path="/login" component={LoginForm} />
+                <PublicRoute exact path="/signup" component={SignupForm} />
             </Switch>
         </Firebase>
     </BrowserRouter>
