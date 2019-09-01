42c5153e6ddf8b89b0496b14db00930a9c5b0571
okradzemirian <okradzemirian@gmail.com>
2019-09-01 19:42:28 +0400

    Configured Apollo Client

--- client/src/router/Router.js
+++ client/src/router/Router.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter, Switch } from 'react-router-dom'
 import Firebase from '../components/Firebase/Firebase'
 import PrivateRoute from './PrivateRoute'
@@ -6,19 +10,31 @@ import PublicRoute from './PublicRoute'
 import Home from '../components/Home/Home'
 import LoginAndSignupPage from '../pages/LoginAndSignupPage/LoginAndSignupPage'
 
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql',
+    credentials: 'include',
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+})
+
 const Router = () => (
-    <BrowserRouter>
-        <Firebase>
-            <Switch>
-                <PrivateRoute exact path="/" component={Home} />
-                <PublicRoute
-                    exact
-                    path="/login"
-                    component={LoginAndSignupPage}
-                />
-            </Switch>
-        </Firebase>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <Firebase>
+                <Switch>
+                    <PrivateRoute exact path="/" component={Home} />
+                    <PublicRoute
+                        exact
+                        path="/login"
+                        component={LoginAndSignupPage}
+                    />
+                </Switch>
+            </Firebase>
+        </BrowserRouter>
+    </ApolloProvider>
 )
 
 export default Router
