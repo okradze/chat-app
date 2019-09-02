6f72effde3c88f046147de1cc17958da54f1d811
okradzemirian <okradzemirian@gmail.com>
2019-09-02 21:48:09 +0400

    Created query for messages in Chat resolver

--- server/src/resolvers/Chat.js
+++ server/src/resolvers/Chat.js
@@ -1,7 +1,12 @@
+import Message from '../models/Message'
+
 const Chat = {
     async users(chat) {
         const { users } = await chat.populate('users').execPopulate()
         return users
+    },
+    async messages(chat) {
+        return await Message.find({ chat: chat._id })
     },
 }
 
